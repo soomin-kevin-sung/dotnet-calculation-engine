f8f688567b6a515ba237c0ada185761272027d8a
soomin-kevin-sung <ssm0725@gmail.com>
2024-02-27 18:03:40 +0900

    adding Interpreter

---
+++ src/CalculationEngine/CalculationEngine/Execution/Interpreter.cs
@@ -0,0 +1,54 @@
+﻿using CalculationEngine.Execution.Interfaces;
+using CalculationEngine.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationEngine.Execution
+{
+	public class Interpreter : IExecutor
+	{
+		public Interpreter()
+			: this(false)
+		{
+
+		}
+
+		public Interpreter(bool caseSensitive)
+		{
+			_caseSensitive = caseSensitive;
+		}
+
+		#region Private Variables
+
+		bool _caseSensitive;
+
+		#endregion
+
+		#region Public Methods
+
+		public Func<IDictionary<string, object>, object> BuildFormula(Operation operation, IFunctionRegistry functionRegistry, IConstantRegistry constantRegistry)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object Execute(Operation operation, IFunctionRegistry functionRegistry, IConstantRegistry constantRegistry)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object Execute(Operation operation, IFunctionRegistry functionRegistry, IConstantRegistry constantRegistry, IDictionary<string, object> variables)
+		{
+			ArgumentNullException.ThrowIfNull(operation);
+
+			if (operation.GetType() == typeof(FloatingConstant))
+				return null;
+
+			return null;
+		}
+
+		#endregion
+	}
+}
